5ad91a62f09d9832c344a366c9b6561ddc2658aa
John O Agure <josephogonji@hotmail.com>
2022-02-05 05:51:31 +0300

    third

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a1302936de82c3e5afcbe9c25428e572fe3bcc5b',
+        'name' => 'chak/mvccore',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'chak/mvccore' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a1302936de82c3e5afcbe9c25428e572fe3bcc5b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
